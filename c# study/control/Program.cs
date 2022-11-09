0ba945b3ed737e5a573dec9b9a22701f48fdd7cb
Milena <emilieguryeva@gmail.com>
2022-11-09 08:20:11 +0300

    написали программу:)

--- c# study/control/Program.cs
+++ c# study/control/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// зададим массив из пяти элементов, которые пользователь введет с клавиатуры
+
+string[] array = new string[5];
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите что-то: ");
+    array[i] = Convert.ToString(Console.ReadLine()!);
+}
+
+string[] resultArray = new string[array.Length];
+int k = 0;
+
+for(int j = 0; j < array.Length; j ++)
+{
+    if(array[j].Length < 4)
+    {
+        resultArray[k] = array[j];
+        k++; 
+    }
+    
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Ниже представлены элементы,  длина которыч меньше или равна 3");
+PrintArray(resultArray);
